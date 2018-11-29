860e7daa8759ebda7fa31340b9a1867d59c95bea
Roman Dvornov <rdvornov@gmail.com>
2018-11-29 17:59:19 +0300

    Make `link` view more adaptive

--- client/views/link.js
+++ client/views/link.js
@@ -3,7 +3,17 @@
 export default function(discovery) {
     discovery.view.define('link', function(el, config, data, context) {
         const { content } = config;
-        const { href, text, external } = data || {};
+        let { href, text, external } = data || {};
+
+        if (typeof data === 'string') {
+            href = text = data;
+        }
+
+        if (!text && href) {
+            text = href;
+        } else if (!href && text) {
+            href = text;
+        }
 
         el.href = href;
 
