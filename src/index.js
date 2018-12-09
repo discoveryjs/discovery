701d87dbec70a37f5e8d6eb758b70799f7c1b63d
Roman Dvornov <rdvornov@gmail.com>
2018-12-09 14:28:47 +0300

    Uniform transfering data type

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const readFilePromise = require('util').promisify(fs.readFile);
+const writeFilePromise = require('util').promisify(fs.writeFile);
 const ensureDir = require('./ensure-dir');
 const cfg = require('./config');
 
@@ -12,20 +13,24 @@ function getCacheFilename(modelConfig) {
         : undefined;
 }
 
-function getData(modelConfig) {
+function getData(modelConfig, stringify) {
     const cacheFile = getCacheFilename(modelConfig);
 
     if (cacheFile && fs.existsSync(cacheFile)) {
-        return readFilePromise(cacheFile, 'utf8');
+        return readFilePromise(cacheFile, 'utf8')
+            .then(data => stringify ? data : JSON.parse(data));
     }
 
     return collectData(modelConfig).then(data => {
+        const stringifiedData = stringify || cacheFile ? JSON.stringify(data) : null;
+
+        if (stringify) {
+            data = stringifiedData;
+        }
+
         if (cacheFile) {
-            fs.writeFile(ensureDir(cacheFile), JSON.stringify(data), (err) => {
-                if (err) {
-                    throw new Error(err);
-                }
-            });
+            return writeFilePromise(ensureDir(cacheFile), stringifiedData, 'utf8')
+                .then(() => data);
         }
 
         return data;
