0a06ba13209cd486d2cc2305ebc103d784098bba
Roman Dvornov <rdvornov@gmail.com>
2019-09-04 15:30:49 +0300

    Fix link to documentation in modelfree mode and update build

--- client/pages/default.js
+++ client/pages/default.js
@@ -13,7 +13,7 @@ export default function(discovery) {
                             'badge:{ text: "model free mode" }'
                         ]
                     },
-                    'html:"<p>Running in <b>model free mode</b>, because no config or no models is set up. Please, read <a href=\\"/link-to-documentation\\" href=\\"_blank\\">documention</a> to learn how to set up models."',
+                    'html:"<p>Running in <b>model free mode</b>, because no config or no models is set up. Please, read <a href=\\"https://github.com/discoveryjs/discovery/blob/master/README.md\\" href=\\"_blank\\">documention</a> to learn how to set up models."',
                     'html:"<p>In this mode you can load a data (JSON), via a button in top right corner or via dropping a file on the page.</p>"'
                 ]
             },
