6f64e45c42b6236c80c6c101d2cfb8d84c4c0eca
Roman Dvornov <rdvornov@gmail.com>
2020-05-26 22:47:32 +0200

    Split signature into modules

---
+++ src/views/signature/collect-stat.js
@@ -0,0 +1,87 @@
+export function collectObjectMap(value, expanded, objectStat) {
+    for (let key in value) {
+        if (!hasOwnProperty.call(value, key)) {
+            continue;
+        }
+
+        if (!expanded) {
+            objectStat.properties = null;
+            break;
+        }
+
+        let propMap;
+
+        if (objectStat.dictMode) {
+            propMap = objectStat.dictMode;
+            propMap.count++;
+            propMap.keys.add(key);
+        } else if (objectStat.properties.has(key)) {
+            propMap = objectStat.properties.get(key);
+            propMap.count++;
+        } else {
+            propMap = {
+                count: 1,
+                map: Object.create(null)
+            };
+            objectStat.properties.set(key, propMap);
+        }
+
+        collectStat(value[key], expanded - 1, propMap.map);
+    }
+}
+
+export function collectStat(value, expanded, stat = Object.create(null)) {
+    const type = value === null
+        ? 'null'
+        : Array.isArray(value)
+            ? 'array'
+            : typeof value;
+
+    switch (type) {
+        default:
+            if (type in stat === false) {
+                stat[type] = new Map();
+            }
+
+            stat[type].set(value, (stat[type].get(value) || 0) + 1);
+            break;
+
+        case 'object':
+            if ('object' in stat === false) {
+                stat.object = new Map();
+                stat.object.count = 0;
+                stat.object.properties = new Map();
+                stat.object.dictMode = null;
+                stat.object.sortKeys = false;
+            }
+
+            stat.object.count++;
+
+            if (!stat.object.has(value)) {
+                stat.object.set(value, 1);
+                collectObjectMap(value, expanded, stat.object);
+            } else {
+                stat.object.set(value, stat.object.get(value) + 1);
+            }
+
+            break;
+
+        case 'array':
+            if ('array' in stat === false) {
+                stat.array = new Map();
+                stat.array.count = 0;
+                stat.array.map = Object.create(null);
+            }
+
+            stat.array.count++;
+            stat.array.set(value, (stat.array.get(value) || 0) + 1);
+
+            for (let i = 0; i < value.length; i++) {
+                collectStat(value[i], expanded, stat.array.map);
+            }
+
+            break;
+    }
+
+    return stat;
+}
---
+++ src/views/signature/index.js
@@ -0,0 +1,104 @@
+/* eslint-env browser */
+
+import { collectObjectMap, collectStat } from './collect-stat.js';
+import { renderStat } from './render-stat.js';
+import { renderPropertyDetails, renderTypeDetails } from './render-details.js';
+
+export default function(discovery) {
+    const elementToData = new WeakMap();
+    const clickHandler = ({ target }) => {
+        let action = 'expand';
+        let activeEl = target.closest(`
+            .view-signature .expand,
+            .view-signature [data-action]
+        `);
+
+        if (!activeEl) {
+            return;
+        }
+
+        if (activeEl.dataset.action) {
+            action = activeEl.dataset.action;
+            activeEl = activeEl.parentNode;
+        }
+
+        const data = elementToData.get(activeEl);
+
+        if (data) {
+            const { path, map, offset } = data;
+            const fragment = document.createDocumentFragment();
+
+            switch (action) {
+                case 'sort-keys':
+                    map.sortKeys = !map.sortKeys;
+                    break;
+
+                case 'dict-mode':
+                    if (map.dictMode) {
+                        map.dictMode = null;
+                    } else {
+                        const dictMode = map.dictMode = {
+                            keys: new Set(),
+                            count: 0,
+                            map: Object.create(null)
+                        };
+                        map.forEach((_, value) => {
+                            for (const key in value) {
+                                if (hasOwnProperty.call(value, key)) {
+                                    dictMode.keys.add(key);
+                                    dictMode.count++;
+                                    collectStat(value[key], 1, dictMode.map);
+                                }
+                            }
+                        });
+                    }
+                    break;
+
+                default:
+                    if (map.properties === null) {
+                        map.properties = new Map();
+                        map.forEach((_, value) => collectObjectMap(value, 1, map));
+                    } else {
+                        map.properties = null;
+                    }
+            }
+
+            renderStat(fragment, { object: map }, elementToData, path, offset);
+            activeEl.replaceWith(fragment);
+        }
+    };
+
+    // single event handler for all `signature` view instances
+    document.addEventListener('click', clickHandler, false);
+
+    // signature details popup
+    new discovery.view.Popup({
+        className: 'signature-details',
+        hoverPin: 'trigger-click',
+        hoverTriggers: `
+            .view-signature .property,
+            .view-signature .type
+        `,
+        render: function(popupEl, triggerEl) {
+            const data = elementToData.get(triggerEl);
+
+            switch (data.type) {
+                case 'property':
+                    return renderPropertyDetails(popupEl, data, discovery);
+
+                case 'type':
+                    return renderTypeDetails(popupEl, data, discovery);
+            }
+        }
+    });
+
+    discovery.view.define('signature', function(el, config, data) {
+        const { expanded, path } = config;
+        const stat = collectStat(data, expanded);
+        const normPath = Array.isArray(path)
+            ? path.map(value => typeof value === 'number' ? `pick(${value})` : value)
+            : undefined;
+
+        renderStat(el, stat, elementToData, normPath);
+    });
+}
