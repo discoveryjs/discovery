4edf0c30948d4aefc43c19401d0f32d82c8d8cb9
Roman Dvornov <rdvornov@gmail.com>
2020-09-30 00:12:44 +0200

    Improve usage section layout in playground

--- src/pages/view-playground.js
+++ src/pages/view-playground.js
@@ -1,4 +1,48 @@
+/* eslint-env browser */
 import { jsonStringifyAsJavaScript } from '../core/utils/json.js';
+
+function isTextNode(node) {
+    return Boolean(node && node.nodeType === Node.TEXT_NODE);
+}
+
+function childrenHtml(node, level = '\n') {
+    let res = '';
+
+    for (const child of node.childNodes) {
+        if (!isTextNode(child) && child.previousSibling && !isTextNode(child.previousSibling)) {
+            res += level;
+        }
+
+        res += nodeHtml(child, level);
+    }
+
+    return res;
+}
+
+function nodeHtml(node, level = '\n') {
+    switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+            const [start, end = ''] = node.cloneNode().outerHTML.split(/(?=<\/[^>]+>$)/);
+            return (
+                start +
+                (node.firstChild && !isTextNode(node.firstChild) ? level + '  ' : '') +
+                childrenHtml(node, level + '  ') +
+                (node.lastChild && !isTextNode(node.lastChild) ? level : '') +
+                end
+            );
+
+        case Node.TEXT_NODE:
+            return node.nodeValue;
+
+        case Node.COMMENT_NODE:
+            return '<!--' + node.nodeValue + '-->';
+
+        case Node.DOCUMENT_FRAGMENT_NODE:
+            return childrenHtml(node, level);
+    }
+
+    return '';
+}
 
 export default function(discovery) {
     const fixture = () => ({
@@ -34,7 +78,7 @@ export default function(discovery) {
                     { when: 'no #.view.name', content: 'text:"Select a view"' },
                     { content: {
                         view: 'context',
-                        data: '.pick(=>name=#.view.name).($name; options.usage.({ $name, usage: $ }))',
+                        data: '$[=>name=#.view.name].($name; options.usage.({ $name, usage: $ }))',
                         content: [
                             'h1:#.view.name',
                             {
@@ -42,17 +86,55 @@ export default function(discovery) {
                                 item: [
                                     'h2:usage.title',
                                     {
-                                        view: 'source',
-                                        data: (data) => ({
-                                            syntax: 'js',
-                                            content: jsonStringifyAsJavaScript(data.usage.view)
-                                        })
-                                    },
-                                    {
-                                        view: 'render',
-                                        config: 'usage.view',
-                                        data: fixture,
-                                        context: '{ view }'
+                                        view: 'context',
+                                        modifiers: {
+                                            view: 'block',
+                                            className: 'usage-render',
+                                            postRender: (el, { onInit }) => onInit(el, 'root'),
+                                            content: {
+                                                view: 'render',
+                                                config: 'usage.view',
+                                                data: fixture,
+                                                context: '{ view }'
+                                            }
+                                        },
+                                        content: {
+                                            view: 'tabs',
+                                            className: 'usage-sources',
+                                            name: 'code',
+                                            tabs: [
+                                                { value: 'config', text: 'Config (JS)' },
+                                                { value: 'config-json', text: 'Config (JSON)' },
+                                                { value: 'html', text: 'HTML' }
+                                            ],
+                                            content: {
+                                                view: 'switch',
+                                                content: [
+                                                    { when: '#.code="config"', content: {
+                                                        view: 'source',
+                                                        className: 'first-tab',
+                                                        data: (data) => ({
+                                                            syntax: 'js',
+                                                            content: jsonStringifyAsJavaScript(data.usage.view)
+                                                        })
+                                                    } },
+                                                    { when: '#.code="config-json"', content: {
+                                                        view: 'source',
+                                                        data: (data) => ({
+                                                            syntax: 'json',
+                                                            content: JSON.stringify(data.usage.view, null, 4)
+                                                        })
+                                                    } },
+                                                    { when: '#.code="html"', content: {
+                                                        view: 'source',
+                                                        data: (data, context) => ({
+                                                            syntax: 'html',
+                                                            content: childrenHtml(context.root)
+                                                        })
+                                                    } }
+                                                ]
+                                            }
+                                        }
                                     }
                                 ]
                             }
