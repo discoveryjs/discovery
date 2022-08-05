c20d449396f3f5d343da84306e335013f1f4b908
Roman Dvornov <rdvornov@gmail.com>
2022-08-05 18:17:44 +0200

    Internal refactoring: the same order for imports & exports

--- src/lib.js
+++ src/lib.js
@@ -1,13 +1,13 @@
-import App from './app/index.js';
+import { version } from './version.js';
 import Widget from './widget/index.js';
+import App from './app/index.js';
 import * as views from './views/index.js';
 import * as pages from './pages/index.js';
 import inspector from './extensions/inspector.js';
 import upload from './extensions/upload.js';
 import router from './extensions/router.js';
-import utils from './core/utils/index.js';
 import { buttons as navButtons } from './nav/index.js';
-import { version } from './version.js';
+import utils from './core/utils/index.js';
 
 export {
     version,
@@ -15,9 +15,9 @@ export {
     App,
     views,
     pages,
-    navButtons,
     inspector,
     upload,
     router,
+    navButtons,
     utils
 };
