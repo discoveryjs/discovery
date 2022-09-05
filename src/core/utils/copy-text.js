18898a96db59fe21272f22d96a582f42f6f503b7
Roman Dvornov <rdvornov@gmail.com>
2022-09-05 18:01:30 +0200

    Refactor copy to clipboard implementation

--- src/core/utils/copy-text.js
+++ src/core/utils/copy-text.js
@@ -16,44 +16,36 @@ const copyTextBufferEl = createElement('div', {
 }, ['text']);
 
 function execCommandFallback(text) {
-    let selection = window.getSelection();
-    let range = document.createRange();
-
-    document.body.appendChild(copyTextBufferEl);
-    copyTextBufferEl.firstChild.nodeValue = text;
-
-    range.selectNodeContents(copyTextBufferEl);
-    selection.removeAllRanges();
-    selection.addRange(range);
+    document.body.append(copyTextBufferEl);
 
     try {
+        const selection = window.getSelection();
+        const range = document.createRange();
+
+        copyTextBufferEl.firstChild.nodeValue = text;
+        range.selectNodeContents(copyTextBufferEl);
+        selection.removeAllRanges();
+        selection.addRange(range);
         document.execCommand('copy');
-    } catch (err) {
-        return Promise.reject(err);
+    } finally {
+        copyTextBufferEl.remove();
     }
-
-    selection.removeAllRanges();
-    copyTextBufferEl.remove();
-
-    return Promise.resolve();
 }
 
 export default async function copyText(text) {
-    if (navigator.clipboard) {
-        let permissionStatus;
+    try {
+        if (navigator.clipboard) {
+            let permissionStatus;
 
-        try {
             permissionStatus = await navigator.permissions.query({
                 name: 'clipboard-write'
             });
-        } catch (_) {
-            return execCommandFallback(text);
-        }
 
-        if (permissionStatus.state === 'granted' || permissionStatus.state === 'prompt') {
-            return navigator.clipboard.writeText(text);
+            if (permissionStatus.state === 'granted' || permissionStatus.state === 'prompt') {
+                return navigator.clipboard.writeText(text);
+            }
         }
-    }
+    } catch (_) {}
 
-    return execCommandFallback(text);
+    execCommandFallback(text);
 }
