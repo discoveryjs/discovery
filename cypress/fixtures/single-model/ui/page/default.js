c90a10442856a810e19e287e2e8c81e1a34c8701
Roman Dvornov <rdvornov@gmail.com>
2023-04-04 21:52:21 +0200

    Temp fix of failed test

--- cypress/fixtures/single-model/ui/page/default.js
+++ cypress/fixtures/single-model/ui/page/default.js
@@ -1,4 +1,15 @@
-discovery.page.define('default', [
-    'h1:#.name',
+// FIXME: temporary fix until @discoveryjs/cli bump to next to 2.4.0 version
+function tempFix(content) {
+    return {
+        view: 'context',
+        modifiers: [
+            (el, config, data, context) => context.model = { name: 'Single model' }
+        ],
+        content
+    };
+}
+
+discovery.page.define('default', tempFix([
+    'h1:#.model.name',
     'struct'
-]);
+]));
