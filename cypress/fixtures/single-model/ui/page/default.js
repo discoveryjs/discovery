e3903d76e9217f68b13a41913f6fc51e2247b974
Roman Dvornov <rdvornov@gmail.com>
2023-04-05 02:59:00 +0200

    Bump @discoveryjs/cli to 2.5.0

--- cypress/fixtures/single-model/ui/page/default.js
+++ cypress/fixtures/single-model/ui/page/default.js
@@ -1,15 +1,4 @@
-// FIXME: temporary fix until @discoveryjs/cli bump to next to 2.4.0 version
-function tempFix(content) {
-    return {
-        view: 'context',
-        modifiers: [
-            (el, config, data, context) => context.model = { name: 'Single model' }
-        ],
-        content
-    };
-}
-
-discovery.page.define('default', tempFix([
+discovery.page.define('default', [
     'h1:#.model.name',
     'struct'
-]));
+]);
