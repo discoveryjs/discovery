f0bb52e097bab96adb0fa851b3a89ceec237fadf
Roman Dvornov <rdvornov@gmail.com>
2023-10-18 03:48:47 +0200

    Update view intro page text

--- src/pages/views-showcase.js
+++ src/pages/views-showcase.js
@@ -3,10 +3,10 @@ import renderUsage from '../views/_usage.js';
 const intro = `
 ## Working with views
 
-A presentation in Discovery.js is setting up as a tree of views.
-There are several ways to define a view, some may be more convenient in one case, others in another.
-The main notation is an object, all other notations are converted to it.
-The only required property is \`view\` which defines a view name, any other properties are optional:
+In Discovery.js, a presentation is set up as a tree of views.
+There are several ways to define a view, each suitable for different cases.
+The primary notation is an object, and all other notations are converted to it.
+The only required property is \`view\`, which defines the view's name. Any other properties are optional.
 
 \`\`\`discovery-view
 {
@@ -16,14 +16,14 @@ The only required property is \`view\` which defines a view name, any other prop
 
 The following properties are supported by any view:
 
-- \`when\` – control view rendering (should it be rendered or not) before an input data is transformed (\`data\` is applied)
-- \`data\` – transform input data for the view and its nested views
-- \`whenData\` – control view rendering after an input data is transformed
-- \`className\` – add class name(s) to the root element of view if any
-- \`postRender\` - a function which should be invoked after view rendering but before placing to destination place in DOM
-- \`tooltip\` – setup a tooltip to show on view hovering, can be applied for views with a container only
+- \`when\`: Controls view rendering (should it be rendered or not) before input data is transformed (\`data\` is applied).
+- \`data\`: Transforms input data for the view and its nested views.
+- \`whenData\`: Controls view rendering after input data is transformed.
+- \`className\`: Adds class name(s) to the root element of the view if any.
+- \`postRender\`: A function to be invoked after view rendering but before placing it in the destination place in the DOM.
+- \`tooltip\`: Sets up a tooltip to show on view hovering; can be applied for views with a container only.
 
-The order of evaluation during a view render:
+The order of evaluation during view rendering:
 
 \`\`\`js
                           input data | output data (the result of "data" evaluation if any)
@@ -32,50 +32,47 @@ render start ---> [when] --> [data] -|-> [whenData] --> [postRender] --> [classN
                                      |
 \`\`\`
 
-When \`data\` property is specified, it changes flow's data according the following rules:
+When the \`data\` property is specified, it changes the flow's data according to the following rules:
 
-- a string – treated as a query which result is used as the output data
-- a function like \`fn(data, context)\` – the result of the function invocation is used as the output data
-- any other value used as the output data
+- A string: Treated as a query whose result is used as the output data.
+- A function like \`fn(data, context)\`: The result of the function invocation is used as the output data.
+- Any other value is used as the output data.
 
-> Note: Only \`when\` and \`data\` properties get an input data, any other functions and queries receive
-a result of \`data\` property evalution. When \`data\` is not specified all the queries and functions
-receive the same input data.
+> Note: Only \`when\` and \`data\` properties receive input data; any other functions and queries receive the result of the \`data\` property evaluation. When \`data\` is not specified, all queries and functions receive the same input data.
 
 Properties \`when\` and \`whenData\` can take:
 
-- a string – treated as a query
-- \`true\` – treated as a empty query which mean that the data itself is examinated with no any transformation
-- \`undefined\` – same when not specified, just render (ignore the property)
-- a function like \`fn(data, context)\`
-- any other value used as is
+- A string: Treated as a query.
+- \`true\`: Treated as an empty query, which means that the data itself is examined with no transformation.
+- \`undefined\`: Same as when not specified; just render (ignore the property).
+- A function like \`fn(data, context)\`.
+- Any other value is used as is.
 
-Regardless of how the value is obtained, it is coerce to a boolean.
-Plase note, that Jora rules are used which mean that empty arrays and object with no own keys are falsy (truthy in JavaScript).
+Regardless of how the value is obtained, it is coerced to a boolean.
+Please note that Jora rules are used, which means that empty arrays and objects with no own keys are falsy (truthy in JavaScript).
 The view is only rendered if the resulting value is truthy.
 
 ## Queries for properties
 
 In some cases, it is necessary to compute the value of a property based on the data.
 To do this, you can use a string value for a property that starts with \`=\`.
-This will mean that everything after the \`=\` is a query which will take output data and produce a value for the property.
+This means that everything after the \`=\` is a query that will take output data and produce a value for the property.
 Other values are passed to the view render as is, without any changes.
 
-In the following, limit number of items rendered at once only if there are more than 12 items:
+For example, limit the number of items rendered at once only if there are more than 12 items:
 
 \`\`\`discovery-view
 {
     view: 'list',
-    limit: '=size() <= 12 and 10'  // the same as '=size() <= 12 ? false : 10'
+    limit: '=size() <= 12 ? false : 10'
 }
 \`\`\`
 
-> Note: In those rare cases when you need to pass to a property a string that starts with \`=\`,
-you can use a query like \`="=some string"\`,<br>e.g. \`{ view: 'example', prop: '="=string="' }\`
+> Note: In those rare cases when you need to pass a property a string that starts with \`=\`, you can use a query like \`"=some string"\`, e.g., \`{ view: 'example', prop: '="=string="' }\`.
 
 ## Shorthand notations
 
-| Shorhand notation | Expands into ... |
+| Shorthand notation | Expands into... |
 | --- | --- |
 | \`'name'\` | \`{ view: 'name' }\`
 | \`'name:<query>'\` | \`{ view: 'name', data: '<query>' }\`
@@ -83,7 +80,7 @@ you can use a query like \`="=some string"\`,<br>e.g. \`{ view: 'example', prop:
 
 ## List of views
 
-If you need to specify a list of views, then you must use an array.
+If you need to specify a list of views, you must use an array.
 An array with view definitions can be passed anywhere a view is accepted as a value.
 
 \`\`\`discovery-view
@@ -101,26 +98,26 @@ An array with view definitions can be passed anywhere a view is accepted as a va
 
 ## Tooltip
 
-A canonical form for a toolup setup is an object with fields (all are optional):
+A canonical form for a tooltip setup is an object with fields (all are optional):
 
 \`\`\`js
 {
     className: 'string',
     position: 'trigger', // 'trigger' or 'pointer' (default)
-    showDelay: false, // true (300ms), false (0ms), a number or a function which takes a triggerEl and returns showDelay value
+    showDelay: false, // true (300ms), false (0ms), a number, or a function which takes a triggerEl and returns showDelay value
     content: 'a view setup'
 }
 \`\`\`
 
-Instead of such object, any view notation can be used, i.e. a string, an object with \`view\` property, an array or a function:
+Instead of such an object, any view notation can be used, i.e., a string, an object with a \`view\` property, an array, or a function:
 
-| Shorhand notation | Expands into ... |
+| Shorthand notation | Expands into... |
 | --- | --- |
 | \`'name'\` | \`{ content: 'name' }\`
 | \`{ view: 'name' }\` | \`{ content: { view: 'name' } }\`
 | \`['view', { view: 'name' }]\` | \`{ content: ['view', { view: 'name' }] }\`
 
-A content is rendering into a popup container. When \`className\` is used it behaves the same as for a view but applies to a popup container.
+The content is rendered into a popup container. When \`className\` is used, it behaves the same as for a view but applies to a popup container.
 `;
 
 export default function(host) {
