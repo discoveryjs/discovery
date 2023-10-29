2b2d58bfe8cc24ef618037c9fb7e8d6620dc5a3b
Roman Dvornov <rdvornov@gmail.com>
2023-10-30 00:02:03 +0100

    Add separate colors for keywords and assertions in a query editor

--- src/views/editor/editor-mode-query.js
+++ src/views/editor/editor-mode-query.js
@@ -1,6 +1,11 @@
 import CodeMirror from 'codemirror';
 
 export default function(config) {
+    const keywords = new Set([
+        'has', 'in', 'no', 'not', 'and', 'or', 'is',
+        'asc', 'ascN', 'ascA', 'ascAN', 'ascNA',
+        'desc', 'descN', 'descA', 'descAN', 'descNA'
+    ]);
     const jsMode = CodeMirror.getMode(config, {
         name: 'javascript',
         json: true
@@ -24,7 +29,48 @@ export default function(config) {
                 return 'variable';
             }
 
-            return jsMode.token(stream, state);
+            let token = jsMode.token(stream, state);
+            const str = stream.string.slice(stream.start, stream.pos);
+
+            if (stream.string[stream.start - 1] !== '.') {
+                if (state.isContext) {
+                    if (token === 'variable' && (state.isContextParen === 0 && str !== 'not')) {
+                        return 'type';
+                    }
+
+                    if (str === '(') {
+                        state.isContextParen++;
+                    } else if (str === ')') {
+                        if (state.isContextParen > 0) {
+                            state.isContextParen--;
+                        }
+                        if (state.isContextParen === 0) {
+                            state.isContext = false;
+                        }
+                    }
+                }
+
+                if (keywords.has(str)) {
+                    if (str === 'is') {
+                        state.isContext = true;
+                        state.isContextParen = 0;
+                    }
+
+                    return 'keyword';
+                }
+            }
+
+            if (state.isContext) {
+                if (token === 'variable') {
+                    return 'type';
+                }
+
+                if (state.isContextParen === 0 && (token || /\S/.test(str))) {
+                    state.isContext = false;
+                }
+            }
+
+            return token;
         }
     };
 }
