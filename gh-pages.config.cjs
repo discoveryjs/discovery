96fcdf34ee723f88fd44f2fa8592eea766ff97e0
Roman Dvornov <rdvornov@gmail.com>
2024-11-04 16:45:17 +0100

    Bump build cli deps

--- gh-pages.config.cjs
+++ gh-pages.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
     name: 'Discovery.js',
     version: require('./package.json').version,
     description: 'Explore, analyze, share',
+    icon: './src/logo.svg',
     view: {
         assets: [
             './gh-pages.extensions.js'
