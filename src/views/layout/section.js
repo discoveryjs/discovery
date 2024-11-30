d556a077ae4af73471816fa6d879180a7872a5f9
Roman Dvornov <rdvornov@gmail.com>
2024-11-30 02:25:49 +0100

    Propagate onInit and onChange handlers in `section` view

--- src/views/layout/section.js
+++ src/views/layout/section.js
@@ -3,11 +3,18 @@ import usage from './section.usage.js';
 
 export default function(host) {
     host.view.define('section', function(el, config, data, context) {
-        const { header, content } = config;
+        const { header, content = [], onInit, onChange } = config;
+        const mixinHandlersIfNeeded = (config) =>
+            typeof onInit !== 'function' && typeof onChange !== 'function'
+                ? config // left as is since nothing to mix in
+                : this.composeConfig(config, {
+                    onInit,
+                    onChange
+                });
 
         return host.view.render(el, [
-            { view: 'header', content: header },
-            content
+            { view: 'header', content: mixinHandlersIfNeeded(header) },
+            mixinHandlersIfNeeded(content)
         ], data, context);
     }, { usage });
 }
